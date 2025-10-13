0917ad9585cfee5e29b192195f8811277feb2655
Mayur Badgujar <mayurbadgujar873@gmail.com>
2025-10-13 19:47:20 +0530

    Datatypes and ECMA standards

---
+++ Day-4/dataTypes.js
@@ -0,0 +1,31 @@
+// "use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ); // we are using nodejs, not brower
+
+console.log(3
+    +
+    3); // code readability should be high
+
+console.log("max");
+
+
+let name = "String"
+let age = 18
+let isLoggedIn = false
+let state; // undefined - not aggissed a value
+let val = null; // empty variable - null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "mayur");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
